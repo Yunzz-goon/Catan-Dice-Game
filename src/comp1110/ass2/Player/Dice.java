763457930599e3e6afa764809d0d2c71e072b59d
Matthew Britton <u7501226@anu.edu.au>
2022-08-24 09:08:07 +1000

    Cindy Second Task Done updated

--- src/comp1110/ass2/Player/Dice.java
+++ src/comp1110/ass2/Player/Dice.java
@@ -9,6 +9,14 @@ public class Dice
     private int dice5;
     private int dice6;
 
+    private int dice1Count;
+    private int dice2Count;
+    private int dice3Count;
+    private int dice4Count;
+    private int dice5Count;
+    private int dice6Count;
+
+
     public Dice()
     {
         dice1 = 0;
@@ -18,6 +26,10 @@ public class Dice
         dice5 = 0;
         dice6 = 0;
     }
+
+    /**
+     * Rolls all the dice
+     */
     public void rollAll()
     {
         dice1 = (int) (Math.random() * 6 + 1);
@@ -28,18 +40,30 @@ public class Dice
         dice6 = (int) (Math.random() * 6 + 1);
     }
 
+    /**
+     * Rolls the dice with the given number - it needs to check if the total number of dice rolls
+     * for the requested dice will be less than or equal to three after this reroll. If it is, then
+     * reroll the specified dice. If it isn't, return the current value of the die.
+     * @param diceNumber the number of the dice to roll
+     */
     public void rollSpecific(int diceNumber)
     {
         switch (diceNumber) {
-            case 1 -> dice1 = (int) (Math.random() * 6 + 1);
-            case 2 -> dice2 = (int) (Math.random() * 6 + 1);
-            case 3 -> dice3 = (int) (Math.random() * 6 + 1);
-            case 4 -> dice4 = (int) (Math.random() * 6 + 1);
-            case 5 -> dice5 = (int) (Math.random() * 6 + 1);
-            case 6 -> dice6 = (int) (Math.random() * 6 + 1);
+            case 1 ->
+            {
+                if (dice1Count < 3)
+                {
+                    dice1 = (int) (Math.random() * 6 + 1);
+                    dice1Count++;
+                }
+            }
         }
     }
 
+    /**
+     * Allows outside classes to access all elements of the dice array
+     * @return the dice array
+     */
     public int[] getDiceArray()
     {
         return new int[]{dice1, dice2, dice3, dice4, dice5, dice6};
