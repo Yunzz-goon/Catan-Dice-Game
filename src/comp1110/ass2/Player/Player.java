ca4778b644b0a0dd86dacfc55e091e1b2a196f06
Matthew Britton <u7501226@anu.edu.au>
2022-08-24 09:59:17 +1000

    End of lab 5 commit

--- src/comp1110/ass2/Player/Player.java
+++ src/comp1110/ass2/Player/Player.java
@@ -4,4 +4,24 @@ public class Player
 {
     private int turnNumber = 0;
     private int playerID;
+
+    public int getTurnNumber()
+    {
+        return turnNumber;
+    }
+
+    public void setTurnNumber(int turnNumber)
+    {
+        this.turnNumber = turnNumber;
+    }
+
+    public int getPlayerID()
+    {
+        return playerID;
+    }
+
+    public void setPlayerID(int playerID)
+    {
+        this.playerID = playerID;
+    }
 }
