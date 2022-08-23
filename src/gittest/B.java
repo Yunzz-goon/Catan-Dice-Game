6a90af09952f5363ea7170a0ea51ea7dee505948
Bob <u7543705@anu.edu.au>
2022-08-24 09:08:45 +1000

    update lab5 testing of step 8

---
+++ src/gittest/B.java
@@ -0,0 +1,15 @@
+package gittest;
+
+import java.sql.SQLOutput;
+
+public class B {
+    @Override
+    public String toString() {
+        return super.toString();
+    }
+
+    public static void main(String[] args) {
+        System.out.println("haha");
+        System.out.println("hahaha");
+    }
+}
