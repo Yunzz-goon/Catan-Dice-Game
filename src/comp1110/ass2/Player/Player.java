50c943ab8cda48dcbe89427e9933deaa47073cbd
Matthew Britton <u7501226@anu.edu.au>
2022-08-24 20:57:05 +1000

    Started some more elements in the Player package

--- src/comp1110/ass2/Player/Player.java
+++ src/comp1110/ass2/Player/Player.java
@@ -2,18 +2,20 @@ package comp1110.ass2.Player;
 
 public class Player
 {
-    private int turnNumber = 0;
+    private int turnNumber;
     private int playerID;
+
+    public Player(int playerID)
+    {
+        this.turnNumber = 0;
+        this.playerID = playerID;
+    }
 
     public int getTurnNumber()
     {
         return turnNumber;
     }
 
-    public void setTurnNumber(int turnNumber)
-    {
-        this.turnNumber = turnNumber;
-    }
 
     public int getPlayerID()
     {
@@ -23,5 +25,22 @@ public class Player
     public void setPlayerID(int playerID)
     {
         this.playerID = playerID;
+    }
+
+    /**
+     * updates the player's turn
+     */
+    public void updateTurn()
+    {
+        this.turnNumber += 1;
+        if (this.turnNumber >= 15) endGame();
+    }
+
+    /**
+     * ends the game after 15 turns
+     */
+    public void endGame()
+    {
+
     }
 }
--- src/comp1110/ass2/Player/Scoring.java
+++ src/comp1110/ass2/Player/Scoring.java
@@ -2,5 +2,10 @@ package comp1110.ass2.Player;
 
 public class Scoring
 {
+    public int calculateFinalScore(String gameState )
+    {
+        return 0;
+    }
+
 
 }
