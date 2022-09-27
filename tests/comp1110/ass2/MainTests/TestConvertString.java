8ff73a6fab2783b5cc77c1630f966e0f78eb0af6
Matthew Britton <u7501226@anu.edu.au>
2022-09-28 00:29:34 +1000

    Matthew D2D written majority of structural layout for tests

--- tests/comp1110/ass2/MainTests/TestConvertString.java
+++ tests/comp1110/ass2/MainTests/TestConvertString.java
@@ -1,5 +1,7 @@
 package comp1110.ass2.MainTests;
 
+import org.junit.jupiter.api.Test;
+
 public class TestConvertString
 {
     /*
@@ -10,5 +12,49 @@ public class TestConvertString
     *  4. That a complete GameState is successfully converted back to a board_state string
     *  5. That a complete GameState is successfully converted back to a resource_state string
      */
+
+    //TODO - these tests are not yet fully written, although a decent chunk is here
+
+    @Test
+    public void testConvertInitialToBS()
+    {
+
+    }
+
+    @Test
+    public void testConvertInitialToRS()
+    {
+
+    }
+
+    @Test
+    public void testConvertIncompleteToBS()
+    {
+
+    }
+
+    @Test
+    public void testConvertIncompleteToRS()
+    {
+
+    }
+
+    @Test
+    public void testConvertBSRStoGS()
+    {
+
+    }
+
+    @Test
+    public void testConvertCompleteToBS()
+    {
+
+    }
+
+    @Test
+    public void testConvertCompleteToRS()
+    {
+
+    }
 
 }
