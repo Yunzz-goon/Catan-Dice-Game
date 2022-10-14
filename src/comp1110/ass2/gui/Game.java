ebb852c42ce53af0db02fe8ca5b8dcd0fe8dfc6c
Yunzhong Zhang <u7543705>
2022-10-14 17:27:17 +1100

    Game inheit from Viewer.

--- src/comp1110/ass2/gui/Game.java
+++ src/comp1110/ass2/gui/Game.java
@@ -1,20 +1,37 @@
 package comp1110.ass2.gui;
 
-import javafx.application.Application;
-import javafx.scene.Group;
 import javafx.scene.Scene;
+import javafx.scene.paint.Color;
 import javafx.stage.Stage;
 
-public class Game extends Application {
+public class Game extends Viewer {
 
-    private final Group root = new Group();
-    private static final int WINDOW_WIDTH = 1200;
-    private static final int WINDOW_HEIGHT = 700;
+//    private final Group root = new Group();
+//    private static final int WINDOW_WIDTH = 1200;
+//    private static final int WINDOW_HEIGHT = 700;
+//    private final Group controls = new Group();
+
+
+
+
 
     @Override
-    public void start(Stage stage) throws Exception {
-        Scene scene = new Scene(this.root, WINDOW_WIDTH, WINDOW_HEIGHT);
-        stage.setScene(scene);
-        stage.show();
+    public void start(Stage primaryStage) throws Exception {
+        primaryStage.setTitle("Board State Viewer");
+
+        buildBuildingFX();
+
+        Scene scene = new Scene(root,WINDOW_WIDTH,WINDOW_HEIGHT, Color.
+                DEEPSKYBLUE);
+
+        root.getChildren().add(controls);
+        makeControls();
+
+        primaryStage.setScene(scene);
+        primaryStage.show();
+    }
+
+    public static void main(String[] args) {
+        launch(args);
     }
 }
